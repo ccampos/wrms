192026562f605dbcffa6c0218ae12d437bcf50cb
Claudio Campos <claudio@claudiocampos.com>
2013-01-11 20:22:40 -0500

    add two colored rectangles intersecting, one with alpha transparency

--- js/app.js
+++ js/app.js
@@ -2,8 +2,23 @@ define([
 	'jquery'
 ], function($) {
 	var initialize = function() {
-		$('canvas#wrms').css('background-color', '#333');
-	}
+		var canvas = $('canvas#wrms')[0];
+
+		if (canvas.getContext) {
+			var ctx = canvas.getContext('2d');
+			// drawing code here
+			$('canvas#wrms').css('background-color', '#ccc');
+			ctx.fillStyle = 'rgb(200, 0, 0)';
+			ctx.fillRect(10, 10, 55, 50);
+
+			ctx.fillStyle = 'rgba(0, 0, 200, 0.5)';
+			ctx.fillRect(30, 30, 55, 50);
+
+		} else {
+			// canvas-unsupported code here
+			$('canvas#wrms').html('canvas-unsupported');
+		};
+	};
 
 	return {
 		initialize: initialize
